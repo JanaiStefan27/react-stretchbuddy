fe87388ea61883db94d98609531d928c710f974a
JanaiStefan27 <janai.stefan122797@gmail.com>
2025-05-22 19:15:02 +0300

    Test push after change repo.

--- src/components/sportcardlist.jsx
+++ src/components/sportcardlist.jsx
@@ -4,14 +4,14 @@ import "./sportcardlist.css";
 
 import basketballImg from "../images/basketball.jpg";
 import handballImg from "../images/handball.jpg";
-import tennisImg from "../images/tenis.jpg";
+import tenisImg from "../images/tenis.jpg";
 
 const SportCardList = () => {
   return (
     <div className="card-scroll-container">
       <SportCard title="Basketball" image={basketballImg} path="/basketball" />
       <SportCard title="Handball" image={handballImg} path="/handball" />
-      <SportCard title="Tenis" image={tennisImg} path="/tenis" />
+      <SportCard title="Tenis" image={tenisImg} path="/tenis" />
     </div>
   );
 };
